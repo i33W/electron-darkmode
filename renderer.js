013d87a0df9725084b7e78b6a4d4f1c914a3033d
i33W <hwangbomee@kakao.com>
2022-01-19 12:20:05 +0900

    electron 기본 화면 생성
    index.html/styles.css/renderer.js
    HTML/CSS/JS 파일들 생성 후
    main.js
    창 열기

---
+++ renderer.js
@@ -0,0 +1,7 @@
+document
+  .getElementById("toggle-dark-mode")
+  .addEventListener("click", () => alert("Toggle Button Click!"));
+
+document
+  .getElementById("reset-to-system")
+  .addEventListener("click", () => alert("System Button Click!"));
